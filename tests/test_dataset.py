c6432c9b1011ea1dc678fc303adb310da863d534
Taeyong Jeon <taeyongjeon24@gmail.com>
2025-03-25 21:25:47 +0900

    feat: add testcase

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,3 +5,8 @@ def test_dataset_len(sample_dataset, sample_dataframe):
 def test_dataset_lter(sample_dataset):
     for row in sample_dataset:
         assert len(row) > 0
+
+
+def test_rolling_dataset_lter(sample_rolling_dataset):
+    for row in sample_rolling_dataset:
+        assert row.shape[0] == sample_rolling_dataset.window
